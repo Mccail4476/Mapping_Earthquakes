1d95c70b282a2a71a4dac2b9dac5413d582bc542
kenneybrian16-ctAfgR <kenneybrian16@gmail.com>
2022-05-13 13:35:02 -0700

    updating

--- Mapping_GeoJSON_Polygons/static/js/logic.js
+++ Mapping_GeoJSON_Polygons/static/js/logic.js
@@ -43,25 +43,18 @@ L.control.layers(baseMaps).addTo(map);
 //let airportData = "https://raw.githubusercontent.com/Mccail4476/Mapping_Earthquakes/main/majorAirports.json";
 
 
-// Create a style for the lines.
-let myStyle = {
-  color: "#ffffa1",
-  weight: 2
-}
+// // Create a style for the lines.
+// let myStyle = {
+//   color: "#ffffa1",
+//   weight: 2
+// }
 
 // Grabbing our GeoJSON data.
-d3.json(torontoData).then(function(data) {
+d3.json(torontoHoods).then(function(data) {
   console.log(data);
 // Creating a GeoJSON layer with the retrieved data.
-L.geoJSON(data, {
-  style: myStyle,
-  onEachFeature: function(feature, layer) {
-    layer.bindPopup("<h3> Airline: " + feature.properties.airline + "</h3> <hr><h3> Destination: "
-    + feature.properties.dst + "</h3>");
-  }
-})
-.addTo(map);
+L.geoJSON(data).addTo(map);
 });
 
-// // Then we add our 'graymap' tile layer to the map.
-light.addTo(map);
+// // // Then we add our 'graymap' tile layer to the map.
+// light.addTo(map);
